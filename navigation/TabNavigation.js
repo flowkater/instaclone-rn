1048ddf42c026a0bbb633286437983030771e44b
flowkater <chjw102@gmail.com>
2019-08-19 16:25:19 +0900

    Initial Navigator

---
+++ navigation/TabNavigation.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+import Home from "../screens/Tabs/Home";
+import Search from "../screens/Tabs/Search";
+import Notifications from "../screens/Tabs/Notifications";
+import Profile from "../screens/Tabs/Profile";
+import MessageLink from "../components/MessageLink";
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InitialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
+
+export default createBottomTabNavigator({
+  Home: {
+    screen: stackFactory(Home, {
+      title: "Home",
+      headerRight: <MessageLink />
+    })
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: "Search"
+    })
+  },
+  Add: {
+    screen: View,
+    navigationOptions: {
+      tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation")
+    }
+  },
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications"
+    })
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile"
+    })
+  }
+});
